4467930ddfbb520a2762950c6d322f67e2fc92e9
adithyaks2005 <adithyasanil11@gmail.com>
2025-01-31 11:00:19 +0530

    updatestudet

--- index.js
+++ index.js
@@ -23,7 +23,28 @@ app.get('/',async(req,res)=>{
     } catch (error) {
      res.send(error);   
     }
-});
+})
+
+app.delete('/remove/:id',async(req,res)=>{
+    console.log(req.params);
+  try {
+    await stuModel.findByIdAndDelete(req.params.id)
+    res.send("Delete successfully!!")
+  } catch (error) {
+    res.send(error.message)
+  }
+})
+
+app.put('/edit/:id',async(req,res)=>{
+  try {
+    var data =await stuModel.findByIdAndUpdate(req.params.id,req.body);
+    res.send("update successfully");
+
+    
+  } catch (error) {
+    res.send(error);
+  }
+})
   
 app.listen(port,(req,res)=>{
     console.log(`server is listening in port ${port}`);
